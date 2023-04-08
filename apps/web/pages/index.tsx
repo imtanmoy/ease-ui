0fed5dbb1bcbbca2d9684c2671421bcec3c787a7
Tanmoy Banik <imtanmoybanik@gmail.com>
2023-04-08 20:50:36 +0600

    web tailwindcss

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -3,7 +3,7 @@ import { Button } from '@imtanmoy/ease-ui';
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <Button>HelloLLLL</Button>
     </div>
   );
